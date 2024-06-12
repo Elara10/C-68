973766c773b85c4ee220584e6c4e87a72776c60f
Isha Kumari <isha02740@gmail.com>
2024-06-12 13:56:49 +0530

    first commit

---
+++ _3.cpp
@@ -0,0 +1,54 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"Hello" <<endl <<"World!";
+//     return 0;
+// }
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int apples;
+//     cin>>apples;
+//     cout<<"Number of apples: "<<apples;
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cin>>a;
+//     cin>>b;
+//     cout<<"Values of a and b are "<<a<<" and "<< b <<endl;
+//     int temp=a;
+//     a=b;
+//     b=temp;
+//     cout<<"Now the value of a and b are "<<a << " and "<<b; 
+// }
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"a:";
+    cin>>a;
+    cout<<"b:";
+    cin>>b;
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"a:"<<a<<endl;
+    cout<<"b:"<<b<<endl;
+}
